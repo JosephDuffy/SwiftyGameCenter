64fed6d2c6810b65fb8024e6c34fbf23a3bf2fc7
Joseph Duffy <JosephDuffy@me.com>
2016-05-30 18:20:07 +0100

    Initial commit

---
+++ SwiftyGameCenter/SwiftyGameCenter.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyGameCenter.h
+//  SwiftyGameCenter
+//
+//  Created by Joseph Duffy on 30/05/2016.
+//  Copyright © 2016 Yetii Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftyGameCenter.
+FOUNDATION_EXPORT double SwiftyGameCenterVersionNumber;
+
+//! Project version string for SwiftyGameCenter.
+FOUNDATION_EXPORT const unsigned char SwiftyGameCenterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyGameCenter/PublicHeader.h>
+
+
